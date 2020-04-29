df9cbfccda82a3967f8817531a5f8951bbf34d89
Mika Shahinyan <mikashahinyan@MacBook-Pro-Mika.local>
2020-04-29 15:50:14 +0400

    remove dummy data

--- src/App.js
+++ src/App.js
@@ -13,18 +13,6 @@ import { Provider } from 'react-redux';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  //console.log(state)
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  
-});
-
-store.dispatch(actionExpenses.addExpense({ description: 'Water Bill', amount: 100, createdAt: 50000 }));
-store.dispatch(actionExpenses.addExpense({ description: 'Gas Bill', amount: 300, createdAt: 20000 }));
-store.dispatch(actionExpenses.addExpense({ description: 'Electricity Bill', amount: 600, createdAt: 40000 }));
-
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
